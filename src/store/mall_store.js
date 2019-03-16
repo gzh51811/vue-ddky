0621dc0ae6b033923838f4942fe417b5cb85524e
WTBIN <2976363944@qq.com>
2019-03-16 11:55:49 +0800

    轮播图使用vuex数据渲染

--- src/store/mall_store.js
+++ src/store/mall_store.js
@@ -2,11 +2,19 @@
 export default {
     state: {
         // mall页面数据 名称为 mallsj
+        mallsj: {
+            data: {
+                mallBannerModule: {
+                    topUrls: []
+                }
+            }
+        }
     },
     mutations: {
         mallsjcc(state, n) {
             // 变更状态
-            state.mallsj = n
+            state.mallsj = n;
+            // console.log("mall页面数据vuex存储完成")
         }
     },
     actions: {
